473c40392420ad594933778d17ba8256f4e33c63
A.I. Kasem <ak89mkii@gmail.com>
2020-10-10 16:43:06 -0400

    Fixed logo

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # path('comics/', views.comics_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+# from .models import Skill,
 
-# Create your views here.
-from django.http import HttpResponse
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+# def skills_index(request):
+#   skills = Skill.objects.filter(user=request.user)
+#   return render(request, 'skills/index.html', { 'skills': skills })
