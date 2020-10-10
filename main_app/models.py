b2eb7ce0b3865008c39dd50a1daea31ad639076b
A.I. Kasem <ak89mkii@gmail.com>
2020-10-10 17:18:13 -0400

    Working on Talent model

--- main_app/models.py
+++ main_app/models.py
@@ -6,9 +6,12 @@ SETTINGS = (
     ('D', 'Night')
 )
 
+LEVELINGS = (
+    ('B', 'Morning'),
+    ('L', 'Noon')
+)
 
-
-class Comic(models.Model):
+class Talent(models.Model):
     skill = models.CharField(max_length=100)
     setting = models.CharField(
         max_length=1,
@@ -17,8 +20,8 @@ class Comic(models.Model):
     )
     leveling = models.CharField(
         max_length=1,
-        choices=SETTINGS,
-        default=SETTINGS[0][0]
+        choices=LEVELINGS,
+        default=LEVELINGS[0][0]
     )
     image = models.CharField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
