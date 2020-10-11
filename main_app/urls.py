60e8d6afe722eaebdc3751734e197a1c18988358
A.I. Kasem <ak89mkii@gmail.com>
2020-10-11 19:38:48 -0400

    Added Add Skill page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('talents/', views.talents_index, name='index'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('talents/create/', views.TalentCreate.as_view(), name='talents_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
 from .models import Talent
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -15,24 +16,25 @@ def about(request):
 @login_required
 def talents_index(request):
     talents = Talent.objects.filter(user=request.user)
-#   talents = Talent.objects.filter(user=request.user)
     return render(request, 'talents/index.html', { 'talents': talents })
+
+class TalentCreate(CreateView):
+  model = Talent
+  fields = ['skill', 'setting', 'leveling', 'image', 'color']
+  def form_valid(self, form):
+    form.instance.user = self.request.user  
+    return super().form_valid(form)
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('index')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
