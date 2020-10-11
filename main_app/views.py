4e7b96810385d3fd11e235986be491f84b27afaa
A.I. Kasem <ak89mkii@gmail.com>
2020-10-10 20:31:40 -0400

    Working on Talent model

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Talent
 
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
 def talents_index(request):
-  talents = Talent.objects.filter(user=request.user)
-  return render(request, 'talents/index.html', { 'talents': talents })
+    talents = Talent.objects.all()
+#   talents = Talent.objects.filter(user=request.user)
+    return render(request, 'talents/index.html', { 'talents': talents })
