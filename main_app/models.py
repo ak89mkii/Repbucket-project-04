95ca737a64ac9d8adc9b88fcced91f179c3748b1
A.I. Kasem <ak89mkii@gmail.com>
2020-10-11 20:28:49 -0400

    Added Add Skill page

--- main_app/models.py
+++ main_app/models.py
@@ -44,3 +44,9 @@ class Talent(models.Model):
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.skill
+
+    def get_absolute_url(self):
+        return reverse('index')
+
