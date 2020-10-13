b45c033459b7052b96db989c04bb0443c970a6c8
A.I. Kasem <ak89mkii@gmail.com>
2020-10-13 14:08:10 -0400

    Added quest paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,4 +15,12 @@ urlpatterns = [
     path('learns/create/', views.LearnCreate.as_view(), name='learns_create'),
     path('learns/<int:pk>/update/', views.LearnUpdate.as_view(), name='learns_update'),
     path('learns/<int:pk>/delete/', views.LearnDelete.as_view(), name='learns_delete'),
+    # quests
+    path('quests/create/', views.QuestCreate.as_view(), name='quests_create'),
+    path('quests/<int:pk>/update/', views.QuestUpdate.as_view(), name='quests_update'),
+    path('quests/<int:pk>/delete/', views.QuestDelete.as_view(), name='quests_delete'),
+    # accepts
+    path('accepts/create/', views.AcceptCreate.as_view(), name='accepts_create'),
+    path('accepts/<int:pk>/update/', views.AcceptUpdate.as_view(), name='accepts_update'),
+    path('accepts/<int:pk>/delete/', views.AcceptDelete.as_view(), name='accepts_delete'),
 ]
